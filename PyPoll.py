e5b82ae92d82f143f958829e15016ce47df36d17
unknown <charly97hernandez@gmail.com>
2021-09-30 18:56:20 -0500

    Added the algorithm to determine the election winner

--- PyPoll.py
+++ PyPoll.py
@@ -21,7 +21,13 @@ candidate_options=[]
 
 # Initialize candidate votes dictionary
 
-candidate_votes ={}
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 #Open the election results and read the file
 
@@ -32,7 +38,6 @@ with open(file_to_load) as election_data:
     # Read the header row.
 
     headers = next(file_reader)
-    print(headers)
 
     # Print each row in the CSV file.
 
@@ -75,29 +80,29 @@ for candidate_name in candidate_votes:
 
     # Print the candidate name and percentage of votes
 
-    print(f'{candidate_name}: received {vote_percentage:.1f}% of the vote.')
+    print(f'{candidate_name}: received {vote_percentage:.1f}% ({votes:,})\n')
 
+    # Determine winning vote count and candidate
+    # Determine if the votes is greater than the winning count.
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+
+         winning_count = votes
+         winning_percentage = vote_percentage
+
+         # And, set the winning_candidate equal to the candidate's name.
+
+         winning_candidate = candidate_name
 # Print the total votes
 
 print(f'The number of total votes is: {total_votes}')
 
-# Print candidate list
-
-print(candidate_options)
-
-# Print the candidate vote dictionary
-
-print(candidate_votes)
-# Using the with statement open the file as a text file.
-
-# with open(file_to_save,'w') as txt_file:
-
-#     #Write some data to the file.
-#     for row in file_reader:
-#         txt_file.write(row+'\n')
-
-# 1. The total number of votes cast
-# 2. A complete list of candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote 
+winning_candidate_summary=(f'-----------------------------\n'
+                        f'Winner: {winning_candidate}\n'
+                        f'Winning Vote Count: {winning_count:,}\n'
+                        f'Winning Percentage: {winning_percentage:.1f}\n'
+                        f'-----------------------------')
+print(winning_candidate_summary)
